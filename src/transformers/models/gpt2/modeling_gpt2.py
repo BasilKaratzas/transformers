c14890bd6024d6d56387249fb097b82b07c43fdb
BasilKaratzas <58182349+BasilKaratzas@users.noreply.github.com>
2021-04-13 02:51:06 +0300

    Update modeling_gpt2.py

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -806,7 +806,7 @@ class GPT2LMHeadModel(GPT2PreTrainedModel):
 
     def __init__(self, config):
         super().__init__(config)
-        print("temp print")
+        print("temp print2")
         self.transformer = GPT2Model(config)
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
